cd70ea2ddaab359f5c580028070e91a411e03471
Camilo-at <cagudelo@mtagraphics.com>
2024-09-04 15:00:21 -0500

    Checkout ui

---
+++ checkout-ui-settings/checkout-ui-custom/checkout6-custom.js
@@ -0,0 +1,157 @@
+$(document).ready(function() {
+    $(window).on("orderFormUpdated.vtex", function(e, t) {
+        calculatePromotions(t);
+    });
+});
+
+var calculatePromotions = e => {
+    var t, a, n = "", s = 0, i = e.storePreferencesData.currencySymbol;
+    if (null != e.ratesAndBenefitsData) {
+        var r = e.ratesAndBenefitsData.rateAndBenefitsIdentifiers, c = e.items;
+        for (let o = 0; o < r.length; o++) {
+            for (let t = s = 0; t < c.length; t++) for (let e = 0; e < c[t].priceTags.length; e++) c[t].priceTags[e].identifier == r[o].id && (s += c[t].priceTags[e].value);
+            t = s.toString().substr(0, s.toString().length - 2), a = s.toString().slice("-2"), 
+            s = new Intl.NumberFormat("es-CO").format(t), n += `
+                <tr class="promotion-row promotion-row-${r[o].id}">
+                    <td class="info">${r[o].name}</td>
+                    <td class="space"></td>
+                    <td class="monetary">${i} ${s},${a}</td>
+                    <td class="empty"></td>
+                </tr>`;
+        }
+        $(".promotion-row").remove(), setTimeout(function() {
+            $(".promotion-row").remove(), $(".totalizers-list").append(n);
+        }, 1e3);
+    }
+};
+
+const CustomCheckout = function() {
+    const o = e => {
+        for (var t = e + "=", o = document.cookie.split(";"), a = 0; a < o.length; a++) {
+            for (var n = o[a]; " " == n.charAt(0); ) n = n.substring(1, n.length);
+            if (0 == n.indexOf(t)) return n.substring(t.length, n.length);
+        }
+        return null;
+    };
+    const a = (e, t) => {
+        console.log("Estoy en guardarDatosMasterData");
+        t = {
+            url: "/api/ds/pub/documents/" + t,
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(e),
+            type: "POST",
+            beforeSend: function() {},
+            success: function(e) {
+                console.log("Successfully Registered Customer");
+            },
+            error: function(e) {
+                console.log("Client Registration Error");
+            },
+            complete: function() {}
+        };
+        $.ajax(t);
+    };
+    return {
+        init: function() {
+            {
+                let e = setInterval(() => {
+                    $("#client-pre-email") && ($("#client-pre-email").attr("placeholder", "Ingresa tu correo electrónico"), 
+                    clearInterval(e));
+                }, 500);
+            }
+            {
+                var e = $(".box-client-info-pj"), t;
+                e.length && (t = "", t += '    <div id="contain-terms">    <p class="terms-conditions dataCheck">        <label for="terms-conditions" class="checkbox check-label label-error">            <input id="terms-conditions" type="checkbox" required>            <span class="btnCheckbox"></span>            <span class="check-text">                He leído y aceptado las <a href="/politicas" target="_blank">políticas de privacidad</a> y <a href="/politicas" target="_blank">Tratamiento de datos Personales</a>            .</span>        </label>        <span class="help error check-error terms-data-error" style="display:none">            Este campo es obligatorio.        </span>    </p>    <p style="margin-top: 10px; font-size: 12px; color: grey">Mediante la aceptación de la Política de Habeas Data y Tratamiento de Datos personales se certifica que el usuario o cliente es una persona mayor de edad (18 años) y podrá ser tratada de acuerdo con la política de tratamiento de datos.</p>    </div>', 
+                $('    <div id="contain-terms">    <p class="terms-conditions dataCheck">        <label for="terms-conditions" class="checkbox check-label label-error">            <input id="terms-conditions" type="checkbox" required>            <span class="btnCheckbox"></span>            <span class="check-text">                He leído y aceptado las <a href="/politicas" target="_blank">políticas de privacidad</a> y <a href="/politicas" target="_blank">Tratamiento de datos Personales</a>            .</span>        </label>        <span class="help error check-error terms-data-error" style="display:none">            Este campo es obligatorio.        </span>    </p>    <p style="margin-top: 10px; font-size: 12px; color: grey">Mediante la aceptación de la Política de Habeas Data y Tratamiento de Datos personales se certifica que el usuario o cliente es una persona mayor de edad (18 años) y podrá ser tratada de acuerdo con la política de tratamiento de datos.</p>    </div>').insertAfter(e), 
+                o("terminosCondiciones") && $("#terms-conditions").attr("checked", !0), 
+                $("#terms-conditions, #authorize-data").change(function() {
+                    var e = $(this);
+                    !1 === e.prop("checked") ? e.parent().next().show() : e.parent().next().hide();
+                }));
+            }
+            setTimeout(function() {
+                const e = document.getElementById("go-to-shipping"), t = document.getElementById("terms-conditions");
+                t.checked = !0, t.addEventListener("click", function() {
+                    !0 === t.checked ? e.classList.remove("disabled") : e.classList.add("disabled");
+                }), $("button#payment-data-submit").on("click", function() {
+                    var e = {
+                        documentNumber: window.API.orderForm.clientProfileData.document,
+                        documentType: "Cédula de Ciudadanía",
+                        firstName: window.API.orderForm.clientProfileData.firstName,
+                        isCorporate: window.API.orderForm.clientProfileData.isCorporate,
+                        lastName: window.API.orderForm.clientProfileData.lastName,
+                        phone: window.API.orderForm.clientProfileData.phone,
+                        email: window.API.orderForm.clientProfileData.email,
+                        acceptTerms: !0
+                    };
+                    Object.values(e).some(e => "" === e) || a(e, "CC");
+                });
+            }, 1e3), setInterval(() => {
+                $(".empty-cart-content").is(":hidden") ? $(".checkout-container >.cart-template .summary-template-holder .summary").css("display", "flex") : $(".checkout-container >.cart-template .summary-template-holder .summary").css("display", "none");
+            }, 500);
+        },
+        pasosCheckout: e => {
+            var t = "cart", o = "profile", a = "shipping", n = "payment", s = "email", i = "orderPlaced", r = ($(".checkoutPasos li").click(function(e) {
+                e.preventDefault(), window.location = "/checkout#/" + $(this).attr("data-step");
+            }), e => {
+                var t = setInterval(() => {
+                    $(".checkoutPasos li").length && (clearInterval(t), $(".checkoutPasos li").removeClass("pasoActivo currentPaso"), 
+                    $('.checkoutPasos li[data-step*="' + e + '"').prevAll().addClass("pasoActivo"), 
+                    $('.checkoutPasos li[data-step*="' + e + '"').addClass("pasoActivo currentPaso"));
+                }, 500);
+            });
+            0 < e.indexOf(t) && r(t), (0 < e.indexOf(s) || 0 < e.indexOf(o)) && r(s), 
+            (0 < e.indexOf(a) || 0 < e.indexOf(n)) && r(a), 0 < e.indexOf(i) && r(i);
+        },
+        changeResolutionImage: () => {
+            "#/shipping" == location.hash && $(".vtex-omnishipping-1-x-image").each(function(e) {
+                $(this).attr("src", $(this).attr("src").replace("-50-50", "-100-100"));
+            });
+        }
+    };
+}(), CustomCheckoutExpress = ($(document).ready(function() {
+    CustomCheckout.init(), CustomCheckout.pasosCheckout(location.pathname + location.hash);
+}), $(document).ajaxStop(function(e) {
+    CustomCheckout.changeResolutionImage();
+}), $(window).on("hashchange", function() {
+    CustomCheckout.changeResolutionImage(), CustomCheckout.pasosCheckout(location.pathname + location.hash);
+}), $(window).on("load", function() {
+    CustomCheckout.changeResolutionImage();
+}), addiAllySlug = "pilatos-ecommerce", $.getScript("https://s3.amazonaws.com/statics.addi.com/vtex/js/vtex-checkout-co.bundle.min.js"), 
+$(".product-item-attachment-offerings-select option:contains('prices')").remove(), 
+$(window).on("load", function() {
+    $(".btn.debit-list-selector").find('option:contains("NEQUI")').hide();
+}), function() {
+    const t = e => e.shippingData.logisticsInfo[0].selectedSla, o = () => {
+        $(".pg-contra-entrega").remove(), $(".payment-group-list-btn a:not(.pg-contra-entrega)").show(), 
+        $(".payment-group-list-btn a:not(.pg-contra-entrega):first-child").click();
+    }, a = () => {
+        $(".pg-contra-entrega, .payment-group-list-btn a").show(), $(".payment-group-list-btn a:first-child").click();
+    };
+    return {
+        init: function() {
+            $(window).on("hashchange load", function() {
+                "#/payment" !== location.hash && "#payment" !== location.hash || vtexjs.checkout.getOrderForm().then(function(e) {
+                    e = e, t(e) && ("pickit - Envío a Domicilio" === t(e) ? (console.log("Entro en Envío a Domicilio Express", t(e)), 
+                    o) : (console.log("Entro en el caso else", t(e)), a))();
+                });
+            });
+        }
+    };
+}());
+
+function isInPaymentRoute() {
+    return [ "#/payment", "/payment" ].includes(window.location.hash);
+}
+
+function handlePaymentMethodChange(e) {
+    isInPaymentRoute() && (e = e.paymentData.payments[0]?.paymentSystem) && ("201" === e ? $("#show-gift-card-group").hide() : $("#show-gift-card-group").show());
+}
+
+$(document).ready(function() {
+    CustomCheckoutExpress.init();
+}), $(window).on("orderFormUpdated.vtex", function(e, t) {
+    handlePaymentMethodChange(t);
+}), vtexjs.checkout.getOrderForm().done(function(e) {
+    handlePaymentMethodChange(e);
+});
---
+++ checkout-ui-settings/src/js/checkout6-custom.js
@@ -0,0 +1,367 @@
+// WARNING: THE USAGE OF CUSTOM SCRIPTS IS NOT SUPPORTED. VTEX IS NOT LIABLE FOR ANY DAMAGES THIS MAY CAUSE. THIS MAY BREAK YOUR STORE AND STOP SALES. IN CASE OF ERRORS, PLEASE DELETE THE CONTENT OF THIS SCRIPT.
+
+//SCRIPT DIA SIN IVA //
+
+$(document).ready(function () {
+  $(window).on('orderFormUpdated.vtex', function (t, e) {
+    calculatePromotions(e)
+  })
+})
+var calculatePromotions = (t) => {
+  var e = '',
+    o = '',
+    r = '',
+    n = 0,
+    a = ((r = 0), t.storePreferencesData.currencySymbol)
+  if (null != t.ratesAndBenefitsData) {
+    var i = t.ratesAndBenefitsData.rateAndBenefitsIdentifiers,
+      s = t.items
+    for (let t = 0; t < i.length; t++) {
+      ;(n = 0), (r = 0)
+      for (let e = 0; e < s.length; e++)
+        for (let o = 0; o < s[e].priceTags.length; o++)
+          s[e].priceTags[o].identifier == i[t].id && (n += s[e].priceTags[o].value)
+      ;(e = n.toString().substr(0, n.toString().length - 2)),
+        (r = n.toString().slice('-2')),
+        (n = new Intl.NumberFormat('es-CO').format(e)),
+        (o += `\n                <tr class="promotion-row promotion-row-${i[t].id}">\n                    <td class="info">${i[t].name}</td>\n                    <td class="space"></td>\n                    <td class="monetary">${a} ${n},${r}</td>\n                    <td class="empty"></td>\n                </tr>`)
+    }
+    $('.promotion-row').remove(),
+      setTimeout(function () {
+        $('.promotion-row').remove(), $('.totalizers-list').append(o)
+      }, 1e3)
+  }
+}
+
+//FINALIZA EL SCRIPT //
+
+const CustomCheckout = (function () {
+  let init = function () {
+    editsOrderForm()
+    checkTerms()
+    validateTerms()
+    editsSummaryCart()
+  }
+
+  const editsOrderForm = () => {
+    let changePlaceHolderEmail = setInterval(() => {
+      if ($('#client-pre-email')) {
+        $('#client-pre-email').attr('placeholder', 'Ingresa tu correo electrónico')
+        clearInterval(changePlaceHolderEmail)
+      }
+    }, 500)
+  }
+
+  const getCookie = (name) => {
+    var nameEQ = name + '='
+    var ca = document.cookie.split(';')
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i]
+      while (c.charAt(0) == ' ') c = c.substring(1, c.length)
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length)
+    }
+    return null
+  }
+
+  const pasosCheckout = (location) => {
+    var statusCheckout = {
+      step1: 'cart',
+      step2: 'profile',
+      step3: 'shipping',
+      step4: 'payment',
+      step5: 'email',
+      step6: 'orderPlaced',
+    }
+
+    $('.checkoutPasos li').click(function (e) {
+      e.preventDefault()
+      window.location = `/checkout#/${$(this).attr('data-step')}`
+    })
+
+    let assignClassStep = (step) => {
+      // console.log(step);
+      var htmlStep = setInterval(() => {
+        if ($('.checkoutPasos li').length) {
+          clearInterval(htmlStep)
+          $('.checkoutPasos li').removeClass('pasoActivo currentPaso')
+          $('.checkoutPasos li[data-step*="' + step + '"')
+            .prevAll()
+            .addClass('pasoActivo')
+          $('.checkoutPasos li[data-step*="' + step + '"').addClass('pasoActivo currentPaso')
+        }
+      }, 500)
+    }
+
+    if (location.indexOf(statusCheckout.step1) > 0) {
+      assignClassStep(statusCheckout.step1)
+    }
+    if (location.indexOf(statusCheckout.step5) > 0 || location.indexOf(statusCheckout.step2) > 0) {
+      assignClassStep(statusCheckout.step5)
+    }
+    if (location.indexOf(statusCheckout.step3) > 0 || location.indexOf(statusCheckout.step4) > 0) {
+      assignClassStep(statusCheckout.step3)
+    }
+    if (location.indexOf(statusCheckout.step6) > 0) {
+      assignClassStep(statusCheckout.step6)
+    }
+  }
+
+  const checkTerms = () => {
+    //let $totalizers = $('.cart-template:not(.mini-cart) .summary-template-holder .cart-totalizers');
+    const $formProfile = $('.box-client-info-pj')
+    if ($formProfile.length) {
+      var newCheck = ''
+      newCheck += '    <div id="contain-terms">'
+      newCheck += '    <p class="terms-conditions dataCheck">'
+      newCheck += '        <label for="terms-conditions" class="checkbox check-label label-error">'
+      newCheck += '            <input id="terms-conditions" type="checkbox" required>'
+      newCheck += '            <span class="btnCheckbox"></span>'
+      newCheck += '            <span class="check-text">'
+      newCheck +=
+        '                He leído y aceptado las <a href="/politicas" target="_blank">políticas de privacidad</a> y <a href="/politicas" target="_blank">Tratamiento de datos Personales</a>'
+      newCheck += '            .</span>'
+      newCheck += '        </label>'
+      newCheck += '        <span class="help error check-error terms-data-error" style="display:none">'
+      newCheck += '            Este campo es obligatorio.'
+      newCheck += '        </span>'
+      newCheck += '    </p>'
+      newCheck +=
+        '    <p style="margin-top: 10px; font-size: 12px; color: grey">Mediante la aceptación de la Política de Habeas Data y Tratamiento de Datos personales se certifica que el usuario o cliente es una persona mayor de edad (18 años) y podrá ser tratada de acuerdo con la política de tratamiento de datos.</p>'
+      newCheck += '    </div>'
+
+      $(newCheck).insertAfter($formProfile)
+
+      if (getCookie('terminosCondiciones')) {
+        $('#terms-conditions').attr('checked', true)
+      }
+
+      $('#terms-conditions, #authorize-data').change(function () {
+        let checkInput = $(this)
+        if (checkInput.prop('checked') === false) {
+          checkInput.parent().next().show()
+        } else {
+          checkInput.parent().next().hide()
+        }
+      })
+    }
+  }
+
+  const validateTerms = () => {
+    setTimeout(function () {
+      const buttonFinalizePurchase = document.getElementById('go-to-shipping')
+      const checkboxTyc = document.getElementById('terms-conditions')
+      checkboxTyc.checked = true
+
+      checkboxTyc.addEventListener('click', function () {
+        if (checkboxTyc.checked === true) {
+          buttonFinalizePurchase.classList.remove('disabled')
+        } else {
+          buttonFinalizePurchase.classList.add('disabled')
+        }
+      })
+
+      $('button#payment-data-submit').on('click', function () {
+        const dataClient = {
+          documentNumber: window.API.orderForm.clientProfileData.document,
+          documentType: 'Cédula de Ciudadanía',
+          firstName: window.API.orderForm.clientProfileData.firstName,
+          isCorporate: window.API.orderForm.clientProfileData.isCorporate,
+          lastName: window.API.orderForm.clientProfileData.lastName,
+          phone: window.API.orderForm.clientProfileData.phone,
+          email: window.API.orderForm.clientProfileData.email,
+          acceptTerms: true,
+        }
+        const validateEmpty = Object.values(dataClient).some((obj) => obj === '')
+
+        if (!validateEmpty) {
+          guardarDatosMasterData(dataClient, 'CC')
+        }
+      })
+    }, 1000)
+  }
+
+  const editsSummaryCart = () => {
+    setInterval(() => {
+      if ($('.empty-cart-content').is(':hidden')) {
+        $('.checkout-container >.cart-template .summary-template-holder .summary').css('display', 'flex')
+      } else {
+        $('.checkout-container >.cart-template .summary-template-holder .summary').css('display', 'none')
+      }
+    }, 500)
+  }
+
+  const changeResolutionImage = () => {
+    if ('#/shipping' == location.hash) {
+      $('.vtex-omnishipping-1-x-image').each(function (element) {
+        $(this).attr('src', $(this).attr('src').replace('-50-50', '-100-100'))
+      })
+    }
+  }
+
+  const guardarDatosMasterData = (datosInputs, entidadMasterData) => {
+    console.log('Estoy en guardarDatosMasterData')
+    var contentPost = {
+      url: '/api/ds/pub/documents/' + entidadMasterData,
+      contentType: 'application/json; charset=utf-8',
+      data: JSON.stringify(datosInputs),
+      type: 'POST',
+      beforeSend: function () {},
+      success: function (data) {
+        console.log('Successfully Registered Customer')
+      },
+      error: function (data) {
+        console.log('Client Registration Error')
+      },
+      complete: function () {},
+    }
+    $.ajax(contentPost)
+  }
+
+  return {
+    init: init,
+    pasosCheckout: pasosCheckout,
+    changeResolutionImage: changeResolutionImage,
+  }
+})()
+
+$(document).ready(function () {
+  CustomCheckout.init()
+  CustomCheckout.pasosCheckout(location.pathname + location.hash)
+})
+$(document).ajaxStop(function (e) {
+  CustomCheckout.changeResolutionImage()
+})
+$(window).on('hashchange', function () {
+  CustomCheckout.changeResolutionImage()
+  CustomCheckout.pasosCheckout(location.pathname + location.hash)
+})
+
+$(window).on('load', function () {
+  CustomCheckout.changeResolutionImage()
+})
+
+addiAllySlug = 'pilatos-ecommerce'
+$.getScript('https://s3.amazonaws.com/statics.addi.com/vtex/js/vtex-checkout-co.bundle.min.js')
+
+$(".product-item-attachment-offerings-select option:contains('prices')").remove()
+
+// Nequi como medio de pago oculto de PSE
+$(window).on('load', function () {
+  $('.btn.debit-list-selector').find('option:contains("NEQUI")').hide()
+})
+
+// Personalización del checkout Express
+const CustomCheckoutExpress = (function () {
+  // Función para obtener el método de envío seleccionado de la orden
+  const selectedSla = (orderForm) => orderForm.shippingData.logisticsInfo[0].selectedSla
+
+  // Oculta el método de pago "Contraentrega" y muestra otros métodos de pago
+  const hidePaymentMethod = () => {
+    // Elimina el método de pago "Contraentrega"
+    $('.pg-contra-entrega').remove()
+    // Muestra todos los métodos de pago excepto "Contraentrega"
+    $('.payment-group-list-btn a:not(.pg-contra-entrega)').show()
+    // Simula un clic en el primer método de pago distinto de "Contraentrega"
+    $('.payment-group-list-btn a:not(.pg-contra-entrega):first-child').click()
+  }
+
+  // Muestra todos los métodos de pago, incluyendo "Contraentrega"
+  const showAllPaymentMethods = () => {
+    // Muestra "Contraentrega" y otros métodos de pago
+    $('.pg-contra-entrega, .payment-group-list-btn a').show()
+    // Simula un clic en el primer método de pago
+    $('.payment-group-list-btn a:first-child').click()
+  }
+
+  // Valida el método de pago seleccionado según el método de envío
+  const validatePaymentMethod = function (orderForm) {
+    if (!selectedSla(orderForm)) {
+      return
+    }
+
+    // Verifica si el método de envío es "Envío a Domicilio Express"
+    if (selectedSla(orderForm) === 'pickit - Envío a Domicilio') {
+      console.log('Entro en Envío a Domicilio Express', selectedSla(orderForm))
+      hidePaymentMethod() // Oculta los métodos de pago según las reglas
+    } else {
+      console.log('Entro en el caso else', selectedSla(orderForm))
+      showAllPaymentMethods() // Muestra todos los métodos de pago
+    }
+  }
+
+  // Inicializa la personalización del checkout
+  const init = function () {
+    $(window).on('hashchange load', function () {
+      // Verifica si la página actual es la página de pago
+      if (location.hash === '#/payment' || location.hash === '#payment') {
+        // Obtiene el formulario de orden y realiza validaciones
+        vtexjs.checkout.getOrderForm().then(function (orderForm) {
+          validatePaymentMethod(orderForm) // Valida el método de pago
+        })
+      }
+    })
+  }
+
+  // Expone la función "init" para iniciar la personalización del checkout
+  return {
+    init: init,
+  }
+})()
+
+// Inicia la personalización del checkout cuando el documento esté listo
+$(document).ready(function () {
+  CustomCheckoutExpress.init()
+})
+// FIN
+
+// OCULTAR EL VALE CUANDO SE SELECCIONA CONTRA ENTREGA
+
+// Función para verificar si estamos en la ruta "/#/payment" o "/#/payment/"
+function isInPaymentRoute() {
+  const validRoutes = ['#/payment', '/payment']
+  return validRoutes.includes(window.location.hash)
+}
+
+// Función para manejar los cambios en el método de pago
+function handlePaymentMethodChange(orderForm) {
+  if (isInPaymentRoute()) {
+    const selectedPaymentMethod = orderForm.paymentData.payments[0]?.paymentSystem
+
+    if (selectedPaymentMethod) {
+      // Verificar si el método de pago seleccionado es igual a "201" (Contraentrega)
+      if (selectedPaymentMethod === '201') {
+        // Ocultar el bloque con el ID "show-gift-card-group" si el método de pago es "Addi"
+        $('#show-gift-card-group').hide()
+      } else {
+        // Mostrar el bloque con el ID "show-gift-card-group" para cualquier otro método de pago
+        $('#show-gift-card-group').show()
+      }
+    }
+  }
+}
+
+// Escuchar el evento "orderFormUpdated" para detectar cambios en el formulario de pedido
+$(window).on('orderFormUpdated.vtex', function (e, orderForm) {
+  handlePaymentMethodChange(orderForm)
+})
+
+// Llamar a la función por primera vez para mostrar el método de pago actual
+vtexjs.checkout.getOrderForm().done(function (orderForm) {
+  handlePaymentMethodChange(orderForm)
+})
+
+// Recoger en tienda que oculte el metodo de pago contra entrega
+//const hideContraEntregaStore = (location) => {
+
+//if (!location.includes('payment')) return
+
+//const slaList = ['Recogida en tienda (1)']
+
+//const { selectedSla } = vtexjs.checkout.orderForm?.shippingData?.logisticsInfo[0]
+
+//if (!selectedSla) return
+
+//const hide = slaList.includes(selectedSla)
+
+//if (hide) $('.pg-contra-entrega').hide();
+//}
