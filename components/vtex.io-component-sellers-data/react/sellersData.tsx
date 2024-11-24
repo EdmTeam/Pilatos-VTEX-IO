b2318eceb6562848953ed13c77e9ff1fa6dd7bf7
webmaster <webmaster@estudiodemoda.com.co>
2024-11-24 11:41:56 -0500

    feat: add indice and id to own and other ship

--- components/vtex.io-component-sellers-data/react/sellersData.tsx
+++ components/vtex.io-component-sellers-data/react/sellersData.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useProduct } from 'vtex.product-context';
+import { useCssHandles } from 'vtex.css-handles'
 
 const sellers = [
   { sellerName: "bscolombia", nit: "805.005.865-7", sic: "805.005.865-7" },
   { sellerName: "AGUA BENDITA S.A.S.", nit: "811044893-1", sic: "811044893-1" },
-  { sellerName: "DOPPLER", nit: "800164923", sic: "800164923" },
+  { sellerName: "DOPPLER", nit: "800164923-9", sic: "800164923-9" },
   { sellerName: "vans", nit: "900.460.312-0", sic: "900.460.312-0" },
   { sellerName: "new era", nit: "900.460.312-0", sic: "900.460.312-0" },
   { sellerName: "jansportc", nit: "900.460.312-0", sic: "900.460.312-0" },
@@ -13,8 +14,16 @@ const sellers = [
   { sellerName: "Estudio de Moda S.A.", nit: "890926803", sic: "890.926.803" }
 ];
 
+const CSS_HANDLES = [
+  'text_sellers_container',
+  'text_sellers'
+] as const
+
 const SellersData = () => {
+
+  const handles = useCssHandles(CSS_HANDLES)
   const context = useProduct();
+  const [isOwnSeller, setIsOwnSeller] = useState(false);
 
   const sellerName = context?.selectedItem?.sellers[0].sellerName;
 
@@ -27,16 +36,22 @@ const SellersData = () => {
   const sellerDetails = sellerName ? findSellerDetails(sellerName): null
 
   useEffect (()=> {
-    console.log("este es el sellerName >>", sellerName)
+      setIsOwnSeller(sellerName === "Estudio de Moda S.A.");
+      console.log("Este es el estado >>", isOwnSeller);
   }, [sellerName])
 
 
   return (
-    <div>
+    <div className={`w-90 pa3 flex justify-center ${handles.text_sellers_conatiner}`}>
       {sellerDetails ? (
-        <p>
+        <>
+        <p className={`w-90 pa3 f7 tc ${handles.text_sellers}`}>
          NIT: {sellerDetails.nit} - SIC: {sellerDetails.sic}
         </p>
+        {isOwnSeller ? <p className={`w-90 pa3 f7 tc dn`} id="ownship"></p>
+        :
+        <p className={`w-90 pa3 f7 tc dn`} id="othership"></p>}
+      </>
       ) : null}
     </div>
   );
