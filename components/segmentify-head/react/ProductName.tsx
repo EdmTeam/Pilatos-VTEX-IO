2e4f684931ec25a603b3e22535ce80999c07d956
davidvaEDM <dfvanegas@mtagraphics.com>
2025-03-23 13:40:40 -0500

    cambio h1 productos

--- components/segmentify-head/react/ProductName.tsx
+++ components/segmentify-head/react/ProductName.tsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import { useProduct } from 'vtex.product-context'
+import styles  from './styles.css'
 
-export default function ProductNameH1() {
+export default function ProductName() {
   const tituloProductoh1 = useProduct()
-
-
-  const suma = (a, b) => a + b
-  console.log(suma(1, 2))
 
   console.log("producto h1" ,tituloProductoh1)
 
   return (
-    <h1>{tituloProductoh1?.product?.productName}</h1>
+    <h1 className={styles.tituloproducto}>{tituloProductoh1?.product?.productName}</h1>
   )
 }
