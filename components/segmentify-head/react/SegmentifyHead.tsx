22a858d56a3130e52a4e272da72c153d33062b7f
davidvaEDM <dfvanegas@mtagraphics.com>
2025-05-12 14:51:32 -0500

    search segmentify

--- components/segmentify-head/react/SegmentifyHead.tsx
+++ components/segmentify-head/react/SegmentifyHead.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import { Helmet } from 'vtex.render-runtime'
-import { useRuntime } from 'vtex.render-runtime';
+// import { useRuntime } from 'vtex.render-runtime';
 
 const SegmentifyHead = () => {
-  const runtime = useRuntime();
+  // const runtime = useRuntime();
 
-  if(runtime.page !== "vtex.store@2.x:store.custom#prueba-head-segmentify") {
-    return null
-  }
+  // if(runtime.page !== "vtex.store@2.x:store.custom#prueba-head-segmentify") {
+  //   return null
+  // }
   
 
   return (
     <>
       <Helmet>
-        <link rel="stylesheet" href="//cdn.segmentify.com/20818070-a3ae-474a-bf22-2e32619ecefb/search.css" />
-        <script src="//cdn.segmentify.com/20818070-a3ae-474a-bf22-2e32619ecefb/search.js"></script>
-      </Helmet>
+
+  <link rel="stylesheet" href="//cdn.segmentify.com/20818070-a3ae-474a-bf22-2e32619ecefb/search.css" />
+
+  <script id='search-pilatos-segmentify' src="//cdn.segmentify.com/20818070-a3ae-474a-bf22-2e32619ecefb/search.js"></script>
+
+</Helmet>
     </>
   );
 };
