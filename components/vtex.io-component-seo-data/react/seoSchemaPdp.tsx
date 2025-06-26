82822ae579111029abc4947d36e63217daa6a5cf
davidvaEDM <dfvanegas@mtagraphics.com>
2025-06-26 16:09:05 -0500

    flag primaton

--- components/vtex.io-component-seo-data/react/seoSchemaPdp.tsx
+++ components/vtex.io-component-seo-data/react/seoSchemaPdp.tsx
@@ -2,26 +2,40 @@ import React, { useState, useEffect } from 'react'
 import { Helmet } from 'vtex.render-runtime'
 import { useProduct } from 'vtex.product-context'
 
+type PdpLdJsonSchema = {
+  productId?: string
+  skuId?: string
+  htmlId: string
+  data: object
+}
+
 type Props = {
-	schemas: PdpLdJsonSchema[]
+  schemas: PdpLdJsonSchema[]
 }
 
 const seoSchemaPdp: StorefrontFunctionComponent<Props> = ({ schemas }) => {
-	const context = useProduct()
-	const [schema, setSchema] = useState<PdpLdJsonSchema | undefined>(undefined);
+  const context = useProduct()
+  const [schema, setSchema] = useState<PdpLdJsonSchema | undefined>(undefined)
 
-	useEffect(() => {
-		if (context?.product) {
-			const id = context?.product.productId;
-			setSchema(schemas.filter((schema: PdpLdJsonSchema) => schema.productId === id)[0]);
-		}
-	}, [context?.product])
+  useEffect(() => {
+    if (context?.product) {
+      const productId = context.product.productId
+      const skuId = context.selectedItem?.itemId
 
-	return schema && schema.data ? (
-		<Helmet>
-			<script type="application/ld+json">{`${JSON.stringify(schema.data)}`}</script>
-		</Helmet>
-	) : null;
+      const matchByProductId = schemas.find(schema => schema.productId === productId)
+      const matchBySkuId = schemas.find(schema => schema.skuId === skuId)
+
+      setSchema(matchByProductId || matchBySkuId)
+    }
+  }, [context?.product, context?.selectedItem])
+
+  return schema && schema.data ? (
+    <Helmet>
+      <script type="application/ld+json" id={schema.htmlId || ''}>
+        {`${JSON.stringify(schema.data)}`}
+      </script>
+    </Helmet>
+  ) : null
 }
 
 export default seoSchemaPdp
